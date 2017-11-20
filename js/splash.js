d634cd988420708e5dd699c71f3f58a2c3522a91
angel917k <angel917k@gmail.com>
2017-11-19 16:59:24 -0800

    created gh-pages

--- js/splash.js
+++ js/splash.js
@@ -28,9 +28,9 @@
 
 
 $(document).ready(function(){
-new CircleType(document.getElementById('curve-text-1')).dir(-1).radius(115); //circular text deccoration 
+new CircleType(document.getElementById('curve-text-1')).dir(-1).radius(115); //circular text deccoration
 
 $('.navbar-toggle').on('click', function() {
-  $('.after-navbar').css('margin-top','230px');  //scroll down entire page on navbar click
+  $('.after-navbar').css('margin-top','230px');  //scroll do wn entire page on navbar click
 });
 });
